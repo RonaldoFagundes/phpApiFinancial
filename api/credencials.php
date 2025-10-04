7876607be138a49f2db639ce6128e0d181277647
RonaldoFagundes <ronaldofagundes@gmail.com>
2025-10-03 22:57:05 -0300

    change db host

--- api/credencials.php
+++ api/credencials.php
@@ -3,11 +3,12 @@
 class Credencials
 {
             
-  private $host = "sql202.byethost15.com";
-  private $user = "b15_39980721";
-  private $password = "!dGRdU@b2vsH3tT";
-  private $db = "b15_39980721_financial_control";
- 
+  
+  private $host = "MYSQL9001.site4now.net";
+  private $user = "abe5ca_control";
+  private $password = "c_th#83dB";
+  private $db = "db_abe5ca_control";
+  
   public function getHost()
   {
     return $this->host;
